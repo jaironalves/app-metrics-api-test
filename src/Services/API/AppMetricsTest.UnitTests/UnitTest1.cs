09774add6059f75fce42df1f1378af52eae0288f
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2023-02-04 16:52:36 -0300

    Adicionando listener

--- src/Services/API/AppMetricsTest.UnitTests/UnitTest1.cs
+++ src/Services/API/AppMetricsTest.UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using App.Metrics;
 using App.Metrics.Filtering;
+using App.Metrics.Meter;
 using AppMetricsTest.API;
 using AppMetricsTest.API.Metrics;
 using AppMetricsTest.API.Metrics.Internal;
@@ -10,6 +11,8 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Patterns;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -55,6 +58,40 @@ namespace AppMetricsTest.UnitTests
         }
 
         [Fact]
+        public void TestListener()
+        {
+            static void OnMeasurementRecorded<T>(Instrument instrument, T measurement, ReadOnlySpan<KeyValuePair<string, object>> tags, object state)
+            {
+                Console.WriteLine($"{instrument.Name} recorded measurement {measurement}");
+            }
+
+            Meter s_meter = new Meter("HatCo.HatStore", "1.0.0");
+            Counter<int> s_hatsSold = s_meter.CreateCounter<int>(name: "hats-sold",
+                                                               unit: "Hats",
+                                                               description: "The number of hats sold in our store");
+
+            using MeterListener meterListener = new MeterListener();
+            meterListener.InstrumentPublished = (instrument, listener) =>
+            {
+                if (instrument.Meter.Name == "HatCo.HatStore")
+                {
+                    listener.EnableMeasurementEvents(instrument);
+                }
+            };
+            meterListener.SetMeasurementEventCallback<int>(OnMeasurementRecorded);
+            meterListener.Start();
+
+            var tags = new KeyValuePair<string, object?>[]
+            {
+                new KeyValuePair<string, object>("tag01", "123"),
+                new KeyValuePair<string, object>("tag02", 3)
+            };
+
+            s_hatsSold.Add(10, tags);
+            s_hatsSold.Add(3, tags);
+        }
+
+        [Fact]
         public void TestMetrics()
         {
             PostRequest postRequest = new PostRequest()
@@ -69,19 +106,19 @@ namespace AppMetricsTest.UnitTests
             mockAccessor.Setup(s => s.PostRequest)
                 .Returns(postRequest);
 
-            IMetrics metrics = AppMetrics.CreateDefaultBuilder()                
+            IMetrics metrics = AppMetrics.CreateDefaultBuilder()
                 .Build();
 
             //var metrics = metricsRoot as IMetrics;
 
-            
+
 
             metrics.IncrementInProgressRequests(mockAccessor.Object);
             metrics.IncrementInProgressRequests(mockAccessor.Object);
             metrics.DecrementInProgressRequests(mockAccessor.Object);
 
             metrics.IncrementInProgressRequests(mockAccessor.Object);
-            metrics.IncrementInProgressRequests(mockAccessor.Object);            
+            metrics.IncrementInProgressRequests(mockAccessor.Object);
             metrics.DecrementInProgressRequests(mockAccessor.Object);
 
             // Assert
